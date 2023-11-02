3008fc35fb056624050dcebaee08d78544bb69aa
userdie <vadim.vadimov123@mail.ru>
2023-11-02 20:08:07 +0500

    1

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import * as yup from "yup";
 import i18next from "./i18n.js";
 import axios from "axios";
 import { uniqueId } from "lodash";
-import { data } from "autoprefixer";
+import { differenceBy } from "lodash";
 
 const state = {
   url: "",
   isValid: false,
-  isLoading: false,
+  isLoaded: false,
+  isCheck: false,
   posts: [],
+  newPosts: [],
   feeds: {
     title: "",
     description: "",
@@ -88,42 +90,55 @@ const promise = (url) => {
     })
 
     .then(function () {
-      const postButtons = document.querySelectorAll(".btn-outline-primary");
-
-      postButtons.forEach((i) =>
-        i.addEventListener("click", () => {
-          document.querySelector(".modal").style = "display: block";
-          document.querySelector(".modal").classList = "modal fade show";
-          state.modalText = document.getElementById(`${i.id}`).textContent;
-          document.querySelector("h5").textContent = state.modalText;
-          document.querySelector(".full-article").href =
-            document.getElementById(`${i.id}`).href;
-          document.getElementById(`${i.id}`).classList =
-            "fw-normal link-secondary";
-          document.querySelector(".text-break").textContent =
-            state.posts[i.id - 1].item.querySelector("description").textContent;
-        })
-      );
-
-      document.querySelectorAll(".fw-bold").forEach((i) =>
-        i.addEventListener("click", () => {
-          document.getElementById(`${i.id}`).classList =
-            "fw-normal link-secondary";
-        })
-      );
-
-      document.querySelector(".btn-secondary").addEventListener("click", () => {
-        document.querySelector(".modal").style = "display: none";
-        document.querySelector(".modal").classList = "modal fade";
-      });
-
-      document.querySelector(".close").addEventListener("click", () => {
-        document.querySelector(".modal").style = "display: none";
-        document.querySelector(".modal").classList = "modal fade";
-      });
+      buttonsLogic();
     });
 };
 
+const buttonsLogic = () => {
+  const postButtons = document.querySelectorAll(".btn-outline-primary");
+
+  postButtons.forEach((i) =>
+    i.addEventListener("click", () => {
+      document.querySelector(".modal").style = "display: block";
+      document.querySelector(".modal").classList = "modal fade show";
+      state.modalText = document.getElementById(`${i.id}`).textContent;
+      document.querySelector("h5").textContent = state.modalText;
+      document.querySelector(".full-article").href = document.getElementById(
+        `${i.id}`
+      ).href;
+      document.getElementById(`${i.id}`).classList = "fw-normal link-secondary";
+      document.querySelector(".text-break").textContent =
+        state.posts[i.id - 1].item.querySelector("description").textContent;
+    })
+  );
+
+  document.querySelectorAll(".fw-bold").forEach((i) =>
+    i.addEventListener("click", () => {
+      document.getElementById(`${i.id}`).classList = "fw-normal link-secondary";
+    })
+  );
+
+  document.querySelector(".btn-secondary").addEventListener("click", () => {
+    document.querySelector(".modal").style = "display: none";
+    document.querySelector(".modal").classList = "modal fade";
+  });
+
+  document.querySelector(".close").addEventListener("click", () => {
+    document.querySelector(".modal").style = "display: none";
+    document.querySelector(".modal").classList = "modal fade";
+  });
+};
+
+const filt = (text) =>
+  state.posts.filter(
+    (el) => el.item.querySelector("title").textContent === text
+  );
+
+const map = () => {
+  return state.posts.map((el) => el.item.querySelector("title").textContent);
+};
+
+// Добавление новых постов
 const checkUpdate = (url) => {
   axios
     .get(`https://allorigins.hexlet.app/get?disableCache=true&url=${url}`)
@@ -132,19 +147,34 @@ const checkUpdate = (url) => {
       parser.parseFromString(response.data.contents, "application/xml")
     )
     .then((data) => {
-      if (data.querySelectorAll("item").length !== state.url) {
-        Array.from(data.querySelectorAll("item")).map((el) =>
-          state.posts.push({ item: el, id: uniqueId() })
-        );
-      }
+      const map1 = Array.from(data.querySelectorAll("item")).map(
+        (el) => el.querySelector("title").textContent
+      );
+      Array.from(data.querySelectorAll("item")).forEach((el) => {
+        if (!map().includes(el.querySelector("title").textContent)) {
+          state.posts.push({ item: el, id: uniqueId() });
+        }
+      });
+      console.log(state.posts);
+    })
+    .then(() => {
+      ul.innerHTML = state.posts
+        .map(
+          (el) =>
+            `<li class='list-group-item d-flex justify-content-between align-items-start border-0 border-end-0'><a id="${
+              el.id
+            }" class="fw-bold" href= ${
+              el.item.querySelector("link").textContent
+            }>${el.item.querySelector("title").textContent}</a><button id="${
+              el.id
+            }" aria-expanded="true" data-bs-toggle="collapse" data-bs-target="#modal" class="btn btn-outline-primary btn-sm">Просмотр</button></li>`
+        )
+        .join("");
+    })
+    .then(() => {
+      buttonsLogic();
     });
-  setTimeout(checkUpdate, 5000, state.url);
-};
-
-const render = (state) => {
-  if (state.isValid === true) {
-    promise(state.url);
-  }
+  setTimeout(checkUpdate, 5000, url);
 };
 
 form.addEventListener("submit", (e) => {
@@ -153,26 +183,44 @@ form.addEventListener("submit", (e) => {
   const formData = new FormData(e.target);
   p.textContent = "";
   let data = formData.get("website");
-  state.url = data;
 
-  schema
-    .validate({ website: data })
-    .catch((errors) => {
-      p.textContent = i18next.t("valid");
-      state.isValid = false;
-      return errors;
-    })
-    .then((data) => {
-      if (data.website !== undefined) {
-        state.isValid = true;
-      }
-    });
-
-  if (state.isValid === true) {
-    state.url = data;
-    state.isLoading = true;
+  if (state.isLoading && state.isCheck) {
+    p.textContent = "RSS уже загружен";
+    p.classList =
+      "feedback m-0 position-absolute small text-success text-danger";
   }
 
-  promise(data);
-  //checkUpdate(data);
+  if (data != state.url) {
+    state.feeds = [];
+    state.posts = [];
+    schema
+      .validate({ website: data })
+      .catch((errors) => {
+        p.textContent = i18next.t("valid");
+        state.isValid = false;
+        return errors;
+      })
+      .then((data) => {
+        if (data.website !== undefined) {
+          state.isValid = true;
+        }
+      })
+      .then(() => {
+        if (state.isValid === true) {
+          state.url = data;
+        }
+      })
+      .then(() => {
+        if (state.isValid === true) {
+          promise(state.url);
+          state.isLoaded = true;
+        }
+      })
+      .then(() => {
+        if (state.isLoaded === true) {
+          checkUpdate(state.url);
+          state.isCheck = true;
+        }
+      });
+  }
 });
